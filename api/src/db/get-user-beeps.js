b543fa51df984272d3388fed94e4302e113ebc34
rdufausansot <rachel.dufau@outlook.fr>
2022-11-30 09:30:28 +0100

    hh

--- api/src/db/get-user-beeps.js
+++
@@ -1,8 +0,0 @@
-import { queryNormalized } from "./connection-pool.js";
-
-export async function getUserBeeps(userId) {
-  return await queryNormalized(
-    "SELECT * FROM beep WHERE author_id = $1 ORDER BY created_at DESC LIMIT 10",
-    [userId]
-  );
-}
